4c9042e5bdd55a28bbd05c43026ddbf9072e32b3
m0nad <102040002+nvac11@users.noreply.github.com>
2024-01-26 02:55:54 -0800

    Add files via upload

--- src/functions.h
+++ src/functions.h
@@ -1,6 +1,21 @@
+#pragma once
 #include <stdbool.h>
-
+#include "datastructures.h"
 void inner_map(int (*f)(int), int * InputOutput, int size);
-bool inner_any(bool (*f)(int), int * Intput, int size);
-bool inner_all(bool (*f)(int), int * Intput, int size);
-void inner_repeat(int * input, int * output, int times, int size);
+bool inner_any(bool (*f)(int), int * Input, int size);
+bool inner_all(bool (*f)(int), int * Input, int size);
+void inner_repeat(int * Output, int * Input, int times, int size);
+void innner_drop(int * Output, int * Input, int nbToDrop, int size);
+void inner_concat(int * Output, int * Input1, int * Input2, int size);
+void inner_scan(int (*f)(int, int), int * Output, int * Input, int size);
+void inner_filter(bool (*f)(int), int * Output, int * Input, int * sizeOutput, int sizeInput);
+void inner_partition(bool (*f)(int), int * Output1, int * Output2, int * Input, int size);
+void inner_zip(tuple * Output, int * Input1, int * Input2, int size);
+void inner_foreach(void (*f)(int), int * Input, int size);
+void inner_sort(bool (*f)(int, int), int * Input, int size);
+void inner_chunk(int ** Output, int * Input, int sizeChunk, int size);
+void inner_generate(int (*f)(int), int * Output, int size);
+int  inner_reduce(int (*f)(int, int), int * Input, int size);
+void inner_zipWithIndex(tuple * Output, int * Input, int size);
+void inner_reverse(int * Output, int * Input, int size);
+void inner_take(int * Output, int * Input, int nbToTake);
